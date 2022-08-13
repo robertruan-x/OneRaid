6378407ba94b3fa5a11dd950de6475a6c157c984
robertruan-x <ruanshixin97@gmail.com>
2022-08-13 17:30:03 +0800

    v0.0.19

--- src/errors/__init__.py
+++ src/errors/__init__.py
@@ -1,15 +1,25 @@
-
 from .example import *
 
+
+# 通过raid类型，转换变量
+def getVarOfRaid(name, raid: str):
+    res = ""
+    if raid != "":
+        try:
+            res = globals()[raid.upper() + name]
+            # exec('res = {}'.format(raid.upper() + name))
+        finally:
+            pass
+    return res
+
+
 class OneRaidError(Exception):
-    def __init__(self, message="") -> None:
+    def __init__(self, message) -> None:
         self._message = message
-    
-    
+
     def message(self):
         return self._message
-    
-    
+
     def __str__(self) -> str:
         return self.message()
 
@@ -18,43 +28,58 @@ class OneRaidParserError(OneRaidError):
     """执行oneraid命令解析错误"""
     pass
 
+
 class OneRaidParserShowError(OneRaidParserError):
 
     def message(self):
         return ONERAID_PARSER_ERROR_CN.format(
             detail_error=self._message,
-            example='\n\t'.join([ONERAID_SHOW_DEFAULT_CN,ONERAID_SHOW_SELECT_CN]))
-    
+            example='\n\t'.join([ONERAID_SHOW_DEFAULT_CN, ONERAID_SHOW_SELECT_CN]))
+
 
 class OneRaidParserCreateRaidError(OneRaidParserError):
     def message(self):
         return ONERAID_PARSER_ERROR_CN.format(
             detail_error=self._message,
-            example='\n\t'.join([ONERAID_CREATE_RAID0_CN,ONERAID_CREATE_RAID1_CN]))
-    
+            example='\n\t'.join([ONERAID_CREATE_RAID0_CN, ONERAID_CREATE_RAID1_CN]))
+
 
 class OneRaidParserDeleteRaidError(OneRaidParserError):
     def message(self):
         return ONERAID_PARSER_ERROR_CN.format(
             detail_error=self._message,
-            example='\n\t'.join([ONERAID_DELETE_SELECT_CN,ONERAID_DELETE_ALL_CN]))
- 
+            example='\n\t'.join([ONERAID_DELETE_SELECT_CN, ONERAID_DELETE_ALL_CN]))
 
 
 class OneRaidCommandError(OneRaidError):
     """命令运行时错误"""
-    pass
+
+    def __init__(self, message, raid) -> None:
+        self._message = message
+        self._raid = raid
 
 
-class OneRaidCommandCreateRaidError(OneRaidError):
+class OneRaidCommandCreateRaidError(OneRaidCommandError):
     def message(self):
-        return ONERAID_PARSER_ERROR_CN.format(
-            detail_error=self._message,
-            example='\n\t'.join([ONERAID_CREATE_RAID0_CN,ONERAID_CREATE_RAID1_CN]))
+        _CREATE_RAID0_CN = getVarOfRaid("_CREATE_RAID0_CN", self._raid)
+        _CREATE_RAID_RECOMMEND_CN = getVarOfRaid("_CREATE_RAID_RECOMMEND_CN", self._raid)
 
+        return ONERAID_COMMAND_ERROR_CN.format(
+            detail_error=self._message,
+            recommend=_CREATE_RAID_RECOMMEND_CN,
+            raw_command='\n\t'.join([_CREATE_RAID0_CN]))
+
+
+class OneRaidCommandGetVdlistError(OneRaidCommandError):
+    def message(self):
+        _GET_VDLIST_CN = getVarOfRaid("_GET_VDLIST_CN", self._raid)
+
+        return ONERAID_COMMAND_ERROR_CN.format(
+            detail_error=self._message,
+            recommend="",
+            raw_command='\n\t'.join([_GET_VDLIST_CN]))
 
 
 class OneRaidDownloadError(OneRaidError):
     """下载命令时错误"""
     pass
-    
--- src/errors/example.py
+++ src/errors/example.py
@@ -1,43 +1,45 @@
 # CN
-ONERAID_SHOW_DEFAULT_CN='one_raid -s  自动识别当前机器的raid卡,展示每张raid卡的信息'
-ONERAID_SHOW_SELECT_CN='one_raid -a 0 -s  指定展示控制器0的raid卡的信息'
+ONERAID_SHOW_DEFAULT_CN = 'one_raid -s  自动识别当前机器的raid卡,展示每张raid卡的信息'
+ONERAID_SHOW_SELECT_CN = 'one_raid -a 0 -s  指定展示控制器0的raid卡的信息'
 
-ONERAID_CREATE_RAID0_CN='one_raid -a 0 -c raid0 "32:12" "AWB"    将控制器0的 32:12 这块磁盘制作成raid0，并且raid参数设置为AWB'
-ONERAID_CREATE_RAID1_CN='one_raid -a 0 -c raid1 "32:11,32:12" "AWB"    将控制器0的 32:11,32:12 这两块磁盘制作成raid1，并且raid参数设置为AWB'
-ONERAID_CREATE_RAID10_CN='one_raid -a 0 -c raid10 "32:11,32:12" "AWB"    将控制器0的 32:11,32:12 这两块磁盘制作成raid10，并且raid参数设置为AWB'
+ONERAID_CREATE_RAID0_CN = 'one_raid -a 0 -c raid0 "32:12" "AWB"    将控制器0的 32:12 这块磁盘制作成raid0，并且raid参数设置为AWB'
+ONERAID_CREATE_RAID1_CN = 'one_raid -a 0 -c raid1 "32:11,32:12" "AWB"    将控制器0的 32:11,32:12 这两块磁盘制作成raid1，并且raid参数设置为AWB'
+ONERAID_CREATE_RAID10_CN = 'one_raid -a 0 -c raid10 "32:11,32:12" "AWB"    将控制器0的 32:11,32:12 这两块磁盘制作成raid10，并且raid参数设置为AWB'
 
-ONERAID_DELETE_SELECT_CN=''
-ONERAID_DELETE_ALL_CN=''
+ONERAID_DELETE_SELECT_CN = ''
+ONERAID_DELETE_ALL_CN = ''
+
+PERCCLI64_CREATE_RAID0_CN = '/tmp/raid/perccli64 /c0 add vd r0 size=all drives=32:13 AWB RA Cache  PERCCLI64将控制器0的 32:12 这块磁盘制作成raid0，并且raid参数设置为AWB RA Cache'
+PERCCLI64_SHOW_PRECACHE_CN = '/tmp/raid/perccli64 /c0 show preservedcache   显示当前已经掉盘，但是保留缓存的磁盘'
+PERCCLI64_SHOW_FOREIGN_CN = '/tmp/raid/perccli64 /c0/fall show  显示当前foreign状态的磁盘'
+PERCCLI64_CREATE_RAID_RECOMMEND_CN = 'perccli64 创建raid前需要先检查当前的磁盘是否存在foreign,以及是否有磁盘存在保留缓存(preservedcache)'
+
+PERCCLI64_GET_VDLIST_CN = '/tmp/raid/perccli64 /c0/vall show    显示制作了raid组的磁盘'
 
 
 
-PERCCLI64_CREATE_RAID0_CN='/tmp/raid/perccli64 /c0 add vd r0 size=all drives=32:13 AWB RA Cache  PERCCLI64将控制器0的 32:12 这块磁盘制作成raid0，并且raid参数设置为AWB RA Cache'
-PERCCLI64_CREATE_RAID_RECOMMAND_CN='perccli64 创建raid需要先检查当前的缓存'
 
-
-ONERAID_PARSER_ERROR_CN="""\
+ONERAID_PARSER_ERROR_CN = """\
     命令参数输入错误, {detail_error}
     
     例子：
         {example}
     """
 
+ONERAID_COMMAND_ERROR_CN = """\
+    命令运行错误, {detail_error}
 
-ONERAID_COMMAND_ERROR_CN="""\
-    命令运行错误, {detail_errror}
-
-    建议：{recommand}
+    建议：{recommend}
 
     或则你可以使用原生raid命令: 
-        {rawcommand}
+        {raw_command}
     """
 
-
-ONERAID_DOWNLOAD_ERROR_CN="""\
-    下载错误, {detail_errror}
+ONERAID_DOWNLOAD_ERROR_CN = """\
+    下载错误, {detail_error}
 
     建议：你需要检查下当前的网络环境，和dns解析
 
     或则你尝试手动执行下载命令: 
-        {rawcommand}
+        {raw_command}
     """
